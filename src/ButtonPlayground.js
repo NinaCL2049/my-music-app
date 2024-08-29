154f866eecc3a6d297990514e3860d5938ec5ce8
NinaCL2049 <ninaclangston@gmail.com>
2024-08-28 19:07:08 -0500

    first commit

---
+++ src/ButtonPlayground.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Button } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
+
+
+const App = () => {
+  return (
+    <div style={{ padding: '50px' }}>
+      <h2>Here's an example of a button using Material UI:</h2>
+      <Button variant="contained" color="secondary" disabled >Click Me</Button>
+      <Button variant="text">Text</Button>
+<Button variant="contained">Contained</Button>
+<Button variant="outlined">Outlined</Button>
+<Button>Primary</Button>
+<Button disabled>Disabled</Button>
+<Button href="#text-buttons">Link</Button>
+<Button variant="contained">Contained</Button>
+<Button variant="contained" disabled>
+  Disabled
+</Button>
+<Button variant="contained" href="#contained-buttons">
+  Link
+</Button>
+<Button variant="contained" disableElevation>
+  Disable elevation
+</Button>
+<Button variant="outlined">Primary</Button>
+<Button variant="outlined" disabled>
+  Disabled
+</Button>
+<Button variant="outlined" href="#outlined-buttons">
+  Link
+</Button>
+<Button
+  onClick={() => {
+    alert('clicked');
+  }}
+>
+  Click me
+</Button>
+<Button color="secondary">Secondary</Button>
+<Button variant="contained" color="success">
+  Success
+</Button>
+<Button variant="outlined" color="error">
+  Error
+</Button>
+<Button variant="outlined" startIcon={<DeleteIcon />}>
+  Delete
+</Button>
+<Button variant="contained" endIcon={<SendIcon />}>
+  Send
+</Button>
+<IconButton aria-label="delete">
+  <DeleteIcon />
+</IconButton>
+<IconButton aria-label="delete" disabled color="primary">
+  <DeleteIcon />
+</IconButton>
+<IconButton color="secondary" aria-label="add an alarm">
+  <AlarmIcon />
+</IconButton>
+<IconButton color="primary" aria-label="add to shopping cart">
+  <AddShoppingCartIcon />
+</IconButton>{/*  */}
+
+    </div>
+  )
+}
+
+export default App;
